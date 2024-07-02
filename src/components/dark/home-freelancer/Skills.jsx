07d3eab05af4b1d7bad803b80a0a7f598c430c5a
Reyansh17 <gahlotreyansh17@gmail.com>
2024-07-02 17:10:50 +0530

    fixeddd

--- src/components/dark/home-freelancer/Skills.jsx
+++ src/components/dark/home-freelancer/Skills.jsx
@@ -1,12 +1,7 @@
-import React, { useEffect } from 'react';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import React from 'react';
+import { motion } from 'framer-motion';
 
 function Skills() {
-  useEffect(() => {
-    AOS.init({ duration: 1000 });
-  }, []);
-
   const imgStyle = {
     display: 'flex',
     justifyContent: 'center',
@@ -23,72 +18,73 @@ function Skills() {
     <div className="my-skills2">
       <div className="container">
         <div className="sec-head text-center mb-80">
-          <h6 className="sub-title main-color mb-15" data-aos="fade-down">Professional Skills</h6>
-          <h2 data-aos="fade-up">
+          <h6 className="sub-title main-color mb-15">Professional Skills</h6>
+          <h2>
             My Skills & <span className="fw-200">Expertise</span>
           </h2>
         </div>
         <div className="row md-marg">
           <div className="col-lg-2 col-md-4 col-6">
-            <div className="item mb-30" data-aos="zoom-in">
+            <motion.div className="item mb-30" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
               <div className="box-bord main-bg">
                 <div className="img" style={imgStyle}>
                   <img src="/dark/assets/imgs/resume/s1.png" alt="UI / UX Design" style={imgInnerStyle} />
                 </div>
               </div>
               <h6 className="fz-18 mt-10 text-center">UI / UX Design</h6>
-            </div>
+            </motion.div>
           </div>
           <div className="col-lg-2 col-md-4 col-6">
-            <div className="item mb-30" data-aos="zoom-in" data-aos-delay="100">
+            <motion.div className="item mb-30" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} transition={{ delay: 0.1 }}>
               <div className="box-bord main-bg">
                 <div className="img" style={imgStyle}>
                   <img src="/dark/assets/imgs/resume/s2.png" alt="Development" style={imgInnerStyle} />
                 </div>
               </div>
               <h6 className="fz-18 mt-10 text-center">Development</h6>
-            </div>
+            </motion.div>
           </div>
           <div className="col-lg-2 col-md-4 col-6">
-            <div className="item mb-30" data-aos="zoom-in" data-aos-delay="200">
+            <motion.div className="item mb-30" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} transition={{ delay: 0.1 }}>
               <div className="box-bord main-bg">
                 <div className="img" style={imgStyle}>
-                  <img src="/dark/assets/imgs/resume/s3.png" alt="Graphic Design" style={imgInnerStyle} />
+                  <img src="/dark/assets/imgs/resume/s3.png" alt="Development" style={imgInnerStyle} />
                 </div>
               </div>
-              <h6 className="fz-18 mt-10 text-center">Graphic Design</h6>
-            </div>
+              <h6 className="fz-18 mt-10 text-center">Graphic Designing</h6>
+            </motion.div>
           </div>
           <div className="col-lg-2 col-md-4 col-6">
-            <div className="item mb-30" data-aos="zoom-in" data-aos-delay="300">
+            <motion.div className="item mb-30" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} transition={{ delay: 0.1 }}>
               <div className="box-bord main-bg">
                 <div className="img" style={imgStyle}>
-                  <img src="/dark/assets/imgs/resume/s5.png" alt="Sketch" style={imgInnerStyle} />
+                  <img src="/dark/assets/imgs/resume/s4.png" alt="Development" style={imgInnerStyle} />
                 </div>
               </div>
-              <h6 className="fz-18 mt-10 text-center">Sketch</h6>
-            </div>
+              <h6 className="fz-18 mt-10 text-center">Digital Marketing</h6>
+            </motion.div>
           </div>
           <div className="col-lg-2 col-md-4 col-6">
-            <div className="item mb-30" data-aos="zoom-in" data-aos-delay="400">
+            <motion.div className="item mb-30" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} transition={{ delay: 0.1 }}>
               <div className="box-bord main-bg">
                 <div className="img" style={imgStyle}>
-                  <img src="/dark/assets/imgs/resume/s4.png" alt="WordPress" style={imgInnerStyle} />
+                  <img src="/dark/assets/imgs/resume/s5.png" alt="Development" style={imgInnerStyle} />
                 </div>
               </div>
-              <h6 className="fz-18 mt-10 text-center">WordPress</h6>
-            </div>
+              <h6 className="fz-18 mt-10 text-center">Embedded Systems</h6>
+            </motion.div>
           </div>
           <div className="col-lg-2 col-md-4 col-6">
-            <div className="item mb-30" data-aos="zoom-in" data-aos-delay="500">
+            <motion.div className="item mb-30" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} transition={{ delay: 0.1 }}>
               <div className="box-bord main-bg">
                 <div className="img" style={imgStyle}>
-                  <img src="/dark/assets/imgs/resume/s6.png" alt="Graphic Design" style={imgInnerStyle} />
+                  <img src="/dark/assets/imgs/resume/s6.png" alt="Development" style={imgInnerStyle} />
                 </div>
               </div>
-              <h6 className="fz-18 mt-10 text-center">Digital M</h6>
-            </div>
+              <h6 className="fz-18 mt-10 text-center">Ai/Ml</h6>
+            </motion.div>
           </div>
+          {/* Add motion.div for each item similarly */}
         </div>
       </div>
     </div>
